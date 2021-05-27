16d26e0fe028e0e1621e754b1d3ca0c19e20ae38
Bruno Oliveira <bruno.oliveira@matera.com>
2021-05-27 11:50:05 -0300

    Validation on contract creation

--- domain/src/main/java/com/bso/companycob/domain/entity/Contract.java
+++ domain/src/main/java/com/bso/companycob/domain/entity/Contract.java
@@ -2,10 +2,10 @@ package com.bso.companycob.domain.entity;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.util.List;
 import java.util.UUID;
 
 import com.bso.companycob.domain.enums.CalcType;
+import com.bso.companycob.domain.exception.DomainException;
 import com.bso.companycob.domain.service.amount.AmountCalculatorDelegate;
 
 public class Contract implements Entity {
@@ -19,7 +19,7 @@ public class Contract implements Entity {
     private final transient AmountCalculatorDelegate amountCalculatorDelegate = new AmountCalculatorDelegate();
 
     public Contract(UUID id, String number, LocalDate date, Bank bank, QuotaCollection quotas, CalcType calcType) {
-        this.id = id;
+        this.id = id == null ? UUID.randomUUID() : id;
         this.number = number;
         this.date = date;
         this.bank = bank;
@@ -45,12 +45,21 @@ public class Contract implements Entity {
         return bank;
     }
 
-    public List<Quota> getQuotas() {
-        return quotas.getQuotas();
+    public QuotaCollection getQuotas() {
+        return quotas;
+    }
+
+    public CalcType getCalcType() {
+        return calcType;
     }
 
     private void validate() {
-        //
+        DomainException.throwsWhen(id == null, "Contract id can't be null");
+        DomainException.throwsWhen(number == null || number.length() == 0, "Contract number can't be null or empty");
+        DomainException.throwsWhen(date == null, "Contract date can't be null");
+        DomainException.throwsWhen(bank == null, "Contract bank can't be null");
+        DomainException.throwsWhen(quotas == null, "Contract quotas can't be null");
+        DomainException.throwsWhen(calcType == null, "Contract calcType can't be null");
     }
 
     public void receivePayment(BigDecimal value) {
--- domain/src/test/java/com/bso/companycob/domain/entity/ContractTest.java
+++ domain/src/test/java/com/bso/companycob/domain/entity/ContractTest.java
@@ -7,39 +7,103 @@ import java.time.LocalDate;
 import java.util.UUID;
 
 import com.bso.companycob.domain.enums.CalcType;
+import com.bso.companycob.domain.exception.DomainException;
 import com.bso.companycob.domain.service.amount.DefaultAmountCalculator;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 class ContractTest {
 
+    private static final String CONTRACT_NUMBER = "XPTO";
+    private static final LocalDate CONTRACT_DATE = LocalDate.now();
+    private static final Bank CONTRACT_BANK = Mockito.mock(Bank.class);
+    private static final QuotaCollection CONTRACT_QUOTAS = Mockito.mock(QuotaCollection.class);
+    private static final CalcType CONTRACT_CALC_TYPE = CalcType.DEFAULT;
+
     @Test
     void testCallingReceivePayment() {
+        Mockito.reset(CONTRACT_BANK, CONTRACT_QUOTAS);
+
         BigDecimal interestRate = BigDecimal.valueOf(2.5);
-        Bank bank = Mockito.mock(Bank.class);
-        Mockito.when(bank.getInterestRate()).thenReturn(interestRate);
+        Mockito.when(CONTRACT_BANK.getInterestRate()).thenReturn(interestRate);
 
-        QuotaCollection quotaCollection = Mockito.mock(QuotaCollection.class);
-        Contract contract = new Contract(UUID.randomUUID(), "123456", LocalDate.now(), bank, quotaCollection, CalcType.DEFAULT);
-
+        Contract contract = createContract();
         contract.receivePayment(BigDecimal.TEN);
 
-        Mockito.verify(quotaCollection, times(1)).updateDebtAmount(Mockito.isA(DefaultAmountCalculator.class), Mockito.eq(interestRate));
-        Mockito.verify(quotaCollection, times(1)).receivePayment(BigDecimal.TEN);
+        Mockito.verify(CONTRACT_QUOTAS, times(1)).updateDebtAmount(Mockito.isA(DefaultAmountCalculator.class), Mockito.eq(interestRate));
+        Mockito.verify(CONTRACT_QUOTAS, times(1)).receivePayment(BigDecimal.TEN);
     }
     
     @Test
     void testCallingUpdateDebtAmount() {
+        Mockito.reset(CONTRACT_BANK, CONTRACT_QUOTAS);
+
         final BigDecimal interestRate = BigDecimal.valueOf(2.5);
-        Bank bank = Mockito.mock(Bank.class);
-        Mockito.when(bank.getInterestRate()).thenReturn(interestRate);
+        Mockito.when(CONTRACT_BANK.getInterestRate()).thenReturn(interestRate);
 
-        QuotaCollection quotaCollection = Mockito.mock(QuotaCollection.class);
-        Contract contract = new Contract(UUID.randomUUID(), "123456", LocalDate.now(), bank, quotaCollection, CalcType.DEFAULT);
-
+        Contract contract = createContract();
         contract.updateDebtAmount();
 
-        Mockito.verify(quotaCollection, times(1)).updateDebtAmount(Mockito.isA(DefaultAmountCalculator.class), Mockito.eq(interestRate));
+        Mockito.verify(CONTRACT_QUOTAS, times(1)).updateDebtAmount(Mockito.isA(DefaultAmountCalculator.class), Mockito.eq(interestRate));
+    }
+
+    @Test
+    void testCreateContractWithSuccess() {
+        UUID id = UUID.randomUUID();
+        var contract = new Contract(id, CONTRACT_NUMBER, CONTRACT_DATE, CONTRACT_BANK, CONTRACT_QUOTAS, CONTRACT_CALC_TYPE);
+        Assertions.assertEquals(id, contract.getId());
+        Assertions.assertEquals(CONTRACT_NUMBER, contract.getNumber());
+        Assertions.assertEquals(CONTRACT_DATE, contract.getDate());
+        Assertions.assertEquals(CONTRACT_BANK, contract.getBank());
+        Assertions.assertEquals(CONTRACT_QUOTAS, contract.getQuotas());
+        Assertions.assertEquals(CONTRACT_CALC_TYPE, contract.getCalcType());
+    }
+
+    @Test
+    void testCreateContractWithIdNullWillCreateUUID() {
+        var contract = new Contract(null, CONTRACT_NUMBER, CONTRACT_DATE, CONTRACT_BANK, CONTRACT_QUOTAS, CONTRACT_CALC_TYPE);
+        Assertions.assertNotNull(contract.getId());
+    }
+
+    @Test
+    void testCreateContractWithNumberNull() {
+        UUID id = UUID.randomUUID();
+        Assertions.assertThrows(DomainException.class, () -> new Contract(id, null, CONTRACT_DATE, CONTRACT_BANK, CONTRACT_QUOTAS, CONTRACT_CALC_TYPE));
+    }
+
+    @Test
+    void testCreateContractWithNumberEmpty() {
+        UUID id = UUID.randomUUID();
+        Assertions.assertThrows(DomainException.class, () -> new Contract(id, "", CONTRACT_DATE, CONTRACT_BANK, CONTRACT_QUOTAS, CONTRACT_CALC_TYPE));
+    }
+
+    @Test
+    void testCreateContractWithDateNull() {
+        UUID id = UUID.randomUUID();
+        Assertions.assertThrows(DomainException.class, () -> new Contract(id, CONTRACT_NUMBER, null, CONTRACT_BANK, CONTRACT_QUOTAS, CONTRACT_CALC_TYPE));
+    }
+
+    @Test
+    void testCreateContractWithBankNull() {
+        UUID id = UUID.randomUUID();
+        Assertions.assertThrows(DomainException.class, () -> new Contract(id, CONTRACT_NUMBER, CONTRACT_DATE, null, CONTRACT_QUOTAS, CONTRACT_CALC_TYPE));
+    }
+
+    @Test
+    void testCreateContractWithQuotasNull() {
+        UUID id = UUID.randomUUID();
+        Assertions.assertThrows(DomainException.class, () -> new Contract(id, CONTRACT_NUMBER, CONTRACT_DATE, CONTRACT_BANK, null, CONTRACT_CALC_TYPE));
+    }
+
+    @Test
+    void testCreateContractWithCalcTypeNull() {
+        UUID id = UUID.randomUUID();
+        Assertions.assertThrows(DomainException.class, () -> new Contract(id, CONTRACT_NUMBER, CONTRACT_DATE, CONTRACT_BANK, CONTRACT_QUOTAS, null));
+    }
+
+    private Contract createContract() {
+        return new Contract(UUID.randomUUID(), CONTRACT_NUMBER, CONTRACT_DATE, CONTRACT_BANK, CONTRACT_QUOTAS, CONTRACT_CALC_TYPE);
     }
 }

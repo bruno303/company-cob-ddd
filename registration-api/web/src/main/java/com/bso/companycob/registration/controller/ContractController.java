b000b20841b59d24f0a4b82364adff09c6ae86c1
Bruno Oliveira <brunooliveira812@gmail.com>
2021-12-01 21:41:29 -0300

    authentication and authorization with keycloak

--- registration-api/web/src/main/java/com/bso/companycob/registration/controller/ContractController.java
+++ registration-api/web/src/main/java/com/bso/companycob/registration/controller/ContractController.java
@@ -5,39 +5,42 @@ import com.bso.companycob.application.service.ContractCrudService;
 import com.bso.companycob.domain.entity.bank.Bank;
 import com.bso.companycob.domain.entity.contract.Contract;
 import com.bso.companycob.domain.entity.contract.Quota;
+import com.bso.companycob.registration.config.CompanyCobController;
 import com.bso.companycob.registration.dto.BankResponse;
 import com.bso.companycob.registration.dto.ContractResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
-@RestController
-@RequestMapping("/api/v1/contract")
+@CompanyCobController
 @RequiredArgsConstructor
 public class ContractController {
 
     private final ContractCrudService contractCrudService;
 
-    @GetMapping
+    @GetMapping("v1/contract")
     public ResponseEntity<List<ContractResponse>> getAllContracts() {
         List<Contract> contracts = contractCrudService.findAll();
         return new ResponseEntity<>(toResponse(contracts), HttpStatus.OK);
     }
 
-    @PostMapping
+    @PostMapping("v1/contract")
     public ResponseEntity<ContractResponse> createContract(@RequestBody ContractCreationDTO dto) {
         Contract contract = contractCrudService.createContract(dto);
         return new ResponseEntity<>(toResponse(contract), HttpStatus.CREATED);
     }
 
     private List<ContractResponse> toResponse(List<Contract> contracts) {
-        List<ContractResponse> result = new ArrayList<>(contracts.size());
-        contracts.forEach(c -> result.add(toResponse(c)));
-        return result;
+        return contracts.stream()
+                .map(this::toResponse)
+                .collect(Collectors.toList());
     }
 
     private ContractResponse toResponse(Contract contract) {

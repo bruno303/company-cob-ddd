5ae909c8f96d2e3f13cf9399cf7178e877131ca5
Bruno Oliveira <brunooliveira812@gmail.com>
2021-12-27 14:15:36 -0300

    fix #10 - configuring postgresql (#11)

---
+++ registration-api/infrastructure/src/main/java/com/bso/companycob/infrastructure/data/config/DataSourceConfig.java
@@ -0,0 +1,28 @@
+package com.bso.companycob.infrastructure.data.config;
+
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+
+@Configuration(proxyBeanMethods = false)
+public class DataSourceConfig {
+
+	@Bean
+	@Primary
+	@ConfigurationProperties("application.datasource")
+	public DataSourceProperties dataSourceProperties() {
+		return new DataSourceProperties();
+	}
+
+	@Bean
+	@Primary
+	@ConfigurationProperties("application.datasource.configuration")
+	public DataSource dataSource(DataSourceProperties properties) {
+		return properties.initializeDataSourceBuilder().build();
+	}
+
+}
--- registration-api/web/src/main/java/com/bso/companycob/registration/controller/BankController.java
+++ registration-api/web/src/main/java/com/bso/companycob/registration/controller/BankController.java
@@ -3,29 +3,31 @@ package com.bso.companycob.registration.controller;
 import com.bso.companycob.application.dto.bank.BankCreationDTO;
 import com.bso.companycob.application.service.BankCrudService;
 import com.bso.companycob.domain.entity.bank.Bank;
+import com.bso.companycob.registration.config.CompanyCobController;
 import com.bso.companycob.registration.dto.BankResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@RestController
-@RequestMapping("/api/v1/bank")
+@CompanyCobController
 @RequiredArgsConstructor
 public class BankController {
 
     private final BankCrudService bankCrudService;
 
-    @GetMapping
+    @GetMapping("v1/bank")
     public ResponseEntity<List<BankResponse>> getAllBanks() {
         List<Bank> all = bankCrudService.findAll();
         return new ResponseEntity<>(toResponse(all), HttpStatus.OK);
     }
 
-    @PostMapping
+    @PostMapping("v1/bank")
     public ResponseEntity<BankResponse> createBank(@RequestBody BankCreationDTO dto) {
         Bank bank = bankCrudService.create(dto);
         return new ResponseEntity<>(toResponse(bank), HttpStatus.CREATED);
